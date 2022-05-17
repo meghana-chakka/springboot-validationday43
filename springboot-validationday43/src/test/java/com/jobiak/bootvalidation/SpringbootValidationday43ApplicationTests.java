74302b4a8c8e38de5aff2e43e96379bfa2ac7fff
chakka Meghana <meghanachakka02@gmail.com>
2022-05-17 11:26:13 +0530

    Completed

---
+++ springboot-validationday43/src/test/java/com/jobiak/bootvalidation/SpringbootValidationday43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobiak.bootvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootValidationday43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
